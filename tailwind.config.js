e6cc5d4a7e516e2f410667b1d72d71da757d9caa
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2024-07-01 12:18:54 +0300

    feat: create initial page

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ["./**/*.{html,js}"], 
+  
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        sans: ["Space Grotesk", "sans-serif"],
+      },
+      colors: {
+        black: "var(--black)",
+        purple: {
+          100: "var(--purple-100)",
+          400: "var(--purple-400)",
+          600: "var(--purple-600)"
+        },
+      },
+       backgroundImage: {
+        "hero-bg": "url('/src/images/allo-book-bg.svg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
